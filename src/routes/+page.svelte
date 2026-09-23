41b6a7e52f14609f1b5754c2b7e93f0acee190c4
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-09-23 10:33:15 +0300

    initial commit
    - working school map with Leaflet using Svelte
    - appropriate initial coordinates & initial zoom level

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+		 integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+		 crossorigin=""/>
+	<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+		 integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+		 crossorigin=""></script>
+	</svelte:head>
+
+<div id="map"></div>
+
+<script>
+import { onMount } from "svelte";
+
+const COORDINATES = [-3.361, 36.613];
+const INITIAL_ZOOM_LEVEL = 18;
+const MAX_ZOOM_LEVEL = 22;
+
+onMount(() => {
+	const map = L.map('map').setView(COORDINATES, INITIAL_ZOOM_LEVEL);
+
+	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: MAX_ZOOM_LEVEL,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+});
+</script>
+
+<style>
+:global(body) {
+	margin: 0;
+}
+#map {
+	height: 100vh;
+}
+</style>
