429c36aac98e6de8d8b8a65ad1a96eb4a808c14c
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-09-25 15:06:35 +0300

    add library polygon
    - the coordinates of the polygon representing the school library is
      defined in `$lib/polygons.ts`
    - when the polygon is clicked, a message will be  logged to the console

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,24 +5,30 @@
 	<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
 		 integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
 		 crossorigin=""></script>
-	</svelte:head>
+</svelte:head>
 
 <div id="map"></div>
 
 <script>
 import { onMount } from "svelte";
+import { LIBRARY } from "$lib/polygons";
 
-const COORDINATES = [-3.361, 36.613];
+// these coordinates & zoom level show most of the school on the screen
+const INITIAL_COORDINATES = [-3.361, 36.613];
 const INITIAL_ZOOM_LEVEL = 18;
 const MAX_ZOOM_LEVEL = 22;
 
 onMount(() => {
-	const map = L.map('map').setView(COORDINATES, INITIAL_ZOOM_LEVEL);
+	const map = L.map('map').setView(INITIAL_COORDINATES, INITIAL_ZOOM_LEVEL);
 
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: MAX_ZOOM_LEVEL,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	}).addTo(map);
+
+	const library = L.polygon(LIBRARY).addTo(map);
+
+	library.on("click", (e) => console.log("library clicked"));
 });
 </script>
 
